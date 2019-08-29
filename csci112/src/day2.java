cf9daa0f1aaa7d5f738c00a1808ba49d6a224986
heba <heba@172.20.10.4>
2019-08-29 16:22:48 -0400

    initial

---
+++ csci112/src/day2.java
@@ -0,0 +1,22 @@
+
+public class day2 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		boolean honesty = false ;
+		
+		if (!honesty)
+		{
+			System.out.println("There is no honesty.");
+		}
+		else
+		{
+			System.out.println("There is honesty");
+		}
+		
+
+	}
+
+}
